6aa66f21572054a83b7c2060e4b209226692d256
Tunjay <thuseynov2020@ada.edu.az>
2018-04-16 02:25:12 +0400

    Updateedildi

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ window.onscroll = function() {myFunction()};
 var bar= document.querySelector(".head");
 
 function myFunction(){
-if(window.pageYOffset>65){
-    bar.setAttribute("style", "position: fixed;")
+if(window.pageYOffset>50){
+    bar.setAttribute("style", "position: fixed; top: 0;")
 }
 else{
-    bar.setAttribute("style", "position: relative;")
+    bar.setAttribute("style", "position: absolute;")
 } 
 }
